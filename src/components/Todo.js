35c19350f8b7c39ea228afa27732cce03404e3be
Pavan Chilukuri <ichilupa@gmail.com>
2019-12-09 20:59:32 -0600

    todo app using react hooks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,55 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./TodoItem";
 import Input from "./Input";
 import Aux from "../hoc/Aux";
 
-export default class Todo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todo: "",
-      todos: [
-        { name: "Buy groceries", done: false },
-        { name: "Pay rent", done: false },
-        { name: "Take tablets", done: false }
-      ]
-    };
-  }
+const Todo = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([
+    { name: "Buy groceries", done: false },
+    { name: "Pay rent", done: false },
+    { name: "Take tablets", done: false }
+  ]);
 
-  handleSave = todo => {
+  // Sent this function as a prop to 'Input' component
+  const handleTodoChange = event => {
+    setTodo(event.target.value);
+  };
+
+  const handleSave = todo => {
     // Slice will copy all the elements from an array and generates a new array
     // Spread operation does the same thing as slice()
     // let newCopiedArray = this.state.todos.slice();
-    let newCopiedArray = [...this.state.todos];
-    newCopiedArray.push({ name: this.state.todo, done: false });
-    this.setState({ todos: newCopiedArray });
+    let newCopiedArray = [...todos];
+    console.log("new copied arr", newCopiedArray);
+    newCopiedArray.push({ name: todo, done: false });
+    setTodos(newCopiedArray);
   };
 
-  // Sent this function as a prop to 'Input' component
-  handleTodoChange = event => {
-    this.setState({ todo: event.target.value });
-  };
+  return (
+    <Aux>
+      <h2>Never miss a thing!</h2>
+      <Input changed={handleTodoChange} />
+      <button onClick={() => handleSave(todo)}>Save</button>
+      <hr />
+      <h2>Your To-Dos</h2>
+      {/* ngFor in Angular ; In React, we use map() */}
 
-  render() {
-    return (
-      <Aux>
-        <h2>Never miss a thing!</h2>
-        <Input changed={this.handleTodoChange} />
-        <button onClick={() => this.handleSave(this.state.todo)}>Save</button>
-        <hr />
-        <h2>Your To-Dos</h2>
-        {/* ngFor in Angular ; In React, we use map() */}
-
-        {/* use slice() for copying the elements from original array 
+      {/* use slice() for copying the elements from original array 
         to the array you want to set else use spread operator */}
 
-        {/* Todos */}
-        {this.state.todos.map((todo, index) => (
-          <div key={todo.name}>
-            <TodoItem todo={todo.name} checked={todo.done} />
-          </div>
-        ))}
-      </Aux>
-    );
-  }
-}
+      {/* Todos */}
+      {todos.map((todo, index) => (
+        <div key={index}>
+          <TodoItem todo={todo.name} checked={todo.done} />
+        </div>
+      ))}
+    </Aux>
+  );
+};
+
+export default Todo;
